bbab8eadbc5747c7361ff04d304c3801e4ecee0a
Udochukwu Kanu <ukanu@unknownd66e064116cd.attlocal.net>
2025-04-17 23:16:00 -0400

    first commit

---
+++ assessment1.playground/Contents.swift
@@ -0,0 +1,48 @@
+
+var abdul:[String:Any]=[:]
+abdul["firstname"]="ab"
+abdul["lastname"]="abdula"
+abdul["age"]=28
+abdul["weight"]=180
+abdul["gender"]="male"
+print(abdul)
+
+var ud:[String:Any]=[:]
+ud["firstname"]="udochukwu"
+ud["lastname"]="kanu"
+ud["age"]=65
+ud["weight"]=235
+ud["gender"]="male"
+print(ud)
+
+var mike:[String:Any]=[:]
+mike["firstname"]="mike"
+mike["lastname"]="tall"
+mike["age"]=24
+mike["weight"]=220
+mike["gender"]="male"
+print(mike)
+
+var hasan:[String:Any]=[:]
+hasan["firstname"]="ha"
+hasan["lastname"]="san"
+hasan["age"]=23
+hasan["weight"]=160
+hasan["gender"]="male"
+print(hasan)
+
+var macel:[String:Any]=[:]
+macel["firstname"]="ma"
+macel["lastname"]="cel"
+macel["age"]=24
+macel["weight"]=160
+macel["gender"]="male"
+print(macel)
+
+var allEmployees:[Any]=[]
+allEmployees.append(abdul)
+allEmployees.append(ud)
+allEmployees.append(mike)
+allEmployees.append(hasan)
+allEmployees.append(macel)
+print(allEmployees)
